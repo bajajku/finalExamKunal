53bdee56e3e363ad22d12eb6d673b177046b40f2
bajajku <bajajku@sheridancollege.ca>
2024-12-06 08:37:48 -0500

    Created Province view model for fetching api response.

---
+++ finalExamKunal/ViewModels/ProvinceViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  MyViewModel.swift
+//  finalExamKunal
+//
+//  Created by Kunal Bajaj on 2024-12-06.
+//
+import Foundation
+import SwiftUI
+
+class ProvinceViewModel : ObservableObject {
+    
+    @Published var provinceData: ProvinceData?
+    
+    @Published var showError = false
+    
+    let baseUrl = "https://mohameom.dev.fast.sheridanc.on.ca/demo/provinces.json"
+    
+    func getProvinceData() {
+                
+        let url = URL(string: baseUrl)
+        
+        let task = URLSession.shared.dataTask(with: url!){ data, response, error in
+            
+            guard error  == nil  else{
+                
+                print("error \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print(" error data not found")
+                return
+            }
+            
+            do {
+                
+                let items = try JSONDecoder().decode(ProvinceData.self, from: data)
+                DispatchQueue.main.async {
+                    self.provinceData = items
+                }
+                
+            }catch {
+                
+                print("error \(error)")
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
+}
