2780fa295f000d68cfd8937716ada4cc65a924ec
bajajku <bajajku@sheridancollege.ca>
2024-12-06 08:24:04 -0500

    Initial Commit

---
+++ finalExamKunal/finalExamKunalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalExamKunalApp.swift
+//  finalExamKunal
+//
+//  Created by Kunal Bajaj on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct finalExamKunalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finalExamKunalTests/finalExamKunalTests.swift
@@ -0,0 +1,17 @@
+//
+//  finalExamKunalTests.swift
+//  finalExamKunalTests
+//
+//  Created by Kunal Bajaj on 2024-12-06.
+//
+
+import Testing
+@testable import finalExamKunal
+
+struct finalExamKunalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
